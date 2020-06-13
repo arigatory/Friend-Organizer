286fdb5384fa0572e55b0a09cd447fb243ead947
Иван Иван <arigatory@gmail.com>
2020-06-13 04:26:26 +0300

    add navigation view

--- FriendOrganizer.UI/Startup/Bootstrapper.cs
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -14,6 +14,7 @@ namespace FriendOrganizer.UI.Startup
             builder.RegisterType<FriendOrganizerDbContext>().AsSelf();
             builder.RegisterType<MainWindow>().AsSelf();
             builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<NavigationViewModel>().As<INavigationViewModel>();
 
             builder.RegisterType<LookupDataService>().AsImplementedInterfaces();
             builder.RegisterType<FriendDataService>().As<IFriendDataService>();
