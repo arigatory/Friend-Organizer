9051480ebc256283f7de6441e9af2c1dedbe63e7
Иван Иван <remillius_dies@outlook.com>
2020-06-21 03:08:55 +0300

    finish 9 validating user input

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -33,6 +33,14 @@ namespace FriendOrganizer.UI.ViewModel
         {
             var friend = await _dataService.GetByIdAsync(friendId);
             Friend = new FriendWrapper(friend);
+            Friend.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(Friend.HasErrors))
+                {
+                    ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+                }
+            };
+            ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
         }
 
         public FriendWrapper Friend
@@ -59,8 +67,8 @@ namespace FriendOrganizer.UI.ViewModel
 
         private bool OnSaveCanExecute()
         {
-            // TODO: Check if friend is valid
-            return true;
+            // TODO: Check in addition if friend has changes
+            return Friend != null && !Friend.HasErrors;
         }
 
         private async void OnOpenFriendDetailView(int friendId)
